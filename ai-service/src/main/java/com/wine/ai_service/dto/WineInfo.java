6f15e95deaaa604743145da01c56f29df3944a32
VittorioAltieri <165920954+VittorioAltieri98@users.noreply.github.com>
2024-06-14 10:11:33 +0200

    Ai-Service controller test Added

--- ai-service/src/main/java/com/wine/ai_service/dto/WineInfo.java
+++ ai-service/src/main/java/com/wine/ai_service/dto/WineInfo.java
@@ -2,6 +2,7 @@ package com.wine.ai_service.dto;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcTypeCode;
@@ -13,6 +14,7 @@ import java.util.Map;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class WineInfo {
 
     private String wineName;
